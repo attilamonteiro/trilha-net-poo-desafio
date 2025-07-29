c9db59d3eddc4167ab307f84e650778a5d8600c8
Attila <attila.abreu@leste.com>
2025-07-29 13:02:24 -0300

    Implement Iphone and Nokia classes inheriting from Smartphone with proper constructors and method overrides

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+// Teste com Nokia
+Smartphone nokia = new Nokia("11999999999", "Nokia 3310", "123456789012345", 32);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Snake");
+
+// Teste com iPhone
+Smartphone iphone = new Iphone("11988888888", "iPhone 13", "987654321098765", 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
